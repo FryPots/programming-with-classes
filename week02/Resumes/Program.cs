81939fbddfa51a1c0b7f204b8c9cff2f4f357e5b
FryPots <martinezserafingilbertoisai@gmail.com>
2026-03-11 17:37:49 -0600

    Finished Resume assignment for week 2

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Cashier";
+        job2._company = "McDonald's";
+        job2._startYear = 2017;
+        job2._endYear = 2018;
+
+        Resume myResume = new Resume();
+        myResume._name = "Gilbertoisai Martinez";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
     }
 }
